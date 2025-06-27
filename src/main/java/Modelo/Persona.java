2da415c31fbccb665e42936d385e6f2abff526c2
Javier Vidaurre <javiervidaurrealmeyda@gmail.com>
2025-06-26 19:54:34 -0500

    Modificado clase abstracta Persona para facilitar herencia.

--- src/main/java/Modelo/Persona.java
+++ src/main/java/Modelo/Persona.java
@@ -8,17 +8,15 @@ public abstract class Persona {
     protected String nombre;
     protected String apellidos;
     protected String dni;
-    protected boolean tieneSIS;
 
     protected Persona() {
     }
 
-    protected Persona(String nombre, String apellidos, String dni, boolean tieneSIS) {
+    protected Persona(String nombre, String apellidos, String dni) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.dni = dni;
-        this.tieneSIS = tieneSIS;
     }
     
-    public abstract <F> void nostrarDatos(F nombre, F apellidos, F dni, F tieneSIS);    
+    protected abstract <T> void nostrarDatos();    
 }
